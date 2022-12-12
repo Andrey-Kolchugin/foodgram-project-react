6569c926d52187a5e55352726c1dd31501cca7dd
Andrey-Kolchugin <a296224@yandex.ru>
2022-12-12 12:02:28 +0300

    15

--- foodgramm/recipes/serializer.py
+++ foodgramm/recipes/serializer.py
@@ -162,7 +162,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
     author = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
-    ingredients = serializers.SlugRelatedField(slug_field=id, queryset=Ingredients.objects.all())
+    ingredients = IngredientsSerializer()
     tags = serializers.ListField(
         child=serializers.SlugRelatedField(
             slug_field='id',
